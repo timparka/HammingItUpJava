bf3f0a0ccc315a28f3a5ef20fd28d02aed5456c3
timothy <pimtarka@gmail.com>
2023-04-14 14:05:36 -0400

    finished lab

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,10 +1,26 @@
 package zipcode;
 
 public class Hamming {
-    public Hamming(String s, String s1) {
+
+    String s = "";
+    String s1 = "";
+
+    public Hamming (String s, String s1) {
+        if (s.length() != s1.length()){
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
+        this.s = s;
+        this.s1 = s1;
     }
 
     public int getHammingDistance() {
-        return -1;
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != s1.charAt(i)) {
+                count++;
+            }
+        }
+        return count;
     }
 }
